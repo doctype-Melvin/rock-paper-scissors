d72614420e8ad7776f076434307313caf3c5d48d
doctype-Melvin <doctype.melvin@gmail.com>
2022-05-12 11:53:44 +0200

    adds styling, icons and cpu choice function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hello world!')
+const RPS = (() => {
+const rps = 
+    [
+        {
+            name: 'rock',
+            icon: '✊',
+            beats: 'scissors'
+        },
+        {
+            name: 'paper',
+            icon: '✋',
+            beats: 'rock'
+        },
+        {
+            name: 'scissors',
+            icon: '🖖',
+            beats: 'paper'
+        }
+    ];
+
+const cpuChoice = rps[Math.floor(Math.random()*rps.length)]
+
+const choices = document.querySelectorAll('.icon')
+console.log(choices)
+})()
