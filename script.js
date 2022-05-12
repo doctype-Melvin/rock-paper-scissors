dd8c78fa855b8b17d092354119b7ba9531c7cc84
doctype-Melvin <doctype.melvin@gmail.com>
2022-05-12 13:46:53 +0200

    starting point for winner function

--- script.js
+++ script.js
@@ -18,8 +18,24 @@ const rps =
         }
     ];
 
-const cpuChoice = rps[Math.floor(Math.random()*rps.length)]
+const cpuChoice = () => rps[Math.floor(Math.random()*rps.length)]
 
-const choices = document.querySelectorAll('.icon')
-console.log(choices)
+const choices = document.querySelectorAll('.icon');
+//Temporary setup with counter to remove event listener
+let count = 0;
+const playerChoice = (e) => {
+    count++
+    if (count === 5) {
+        choices.forEach(button => button.removeEventListener('click', playerChoice))
+    }
+    let choice = rps.find(el => el.name === e.target.id)
+    playRound(choice.name);
+}
+choices.forEach(button => button.addEventListener('click', playerChoice));
+
+const playRound = (player, cpu) => {
+    this.player = player;
+    cpu = cpuChoice();
+}
+
 })()
