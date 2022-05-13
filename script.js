b357f772b57e96433a1112fa433965bf667de24f
doctype-Melvin <doctype.melvin@gmail.com>
2022-05-13 06:30:05 +0200

    adds score display and winner evaluation fn

--- script.js
+++ script.js
@@ -25,17 +25,31 @@ const choices = document.querySelectorAll('.icon');
 let count = 0;
 const playerChoice = (e) => {
     count++
-    if (count === 5) {
+    if (count === 5) {//After 5 rounds remove event listeners
         choices.forEach(button => button.removeEventListener('click', playerChoice))
     }
     let choice = rps.find(el => el.name === e.target.id)
-    playRound(choice.name);
+    
+    playRound(choice);
 }
+//Add event listeners to icons 
 choices.forEach(button => button.addEventListener('click', playerChoice));
 
-const playRound = (player, cpu) => {
+const playRound = (player, cpu) => { //Takes the objects and passes them for winner evaluation
     this.player = player;
     cpu = cpuChoice();
+        let playerWin = whoWins(player, cpu);
+        let cpuWin = whoWins(cpu, player);
+            if(playerWin) {console.log(`Player wins`)}
+            else if(cpuWin) {console.log(`Computer wins`)}
+            else draw()
 }
 
+const whoWins = (winner, loser) => { //Evaluates winner
+    return winner.beats === loser.name
+}
+
+const draw = () => { //Draw scenario
+    console.log(`draw`)
+}
 })()
