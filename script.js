247d510fedb180059056e5c2a0b4611df158f7d9
doctype-Melvin <doctype.melvin@gmail.com>
2022-05-13 08:00:17 +0200

    ends game after exactly 5 wins

--- script.js
+++ script.js
@@ -24,45 +24,73 @@ const choices = document.querySelectorAll('.icon');
 
 //Scores
 const playerScore = document.querySelector('#ply-score');
-const cpuScore = document.querySelector('#cpu-score')
+const cpuScore = document.querySelector('#cpu-score');
+
+//Game display
+const result = document.querySelector('.roundResult');
+const history = document.querySelector('.gameHistory');
 
 //Player choice initiates first round
 const playerChoice = (e) => {
-    if ((parseInt(playerScore.innerText) === 5 && parseInt(cpuScore.innerText) < 5) || 
-        (parseInt(cpuScore.innerText) === 5 && parseInt(playerScore.innerText) < 5)) {//After 5 wins remove event listeners
-        choices.forEach(button => button.removeEventListener('click', playerChoice))
-    }
     let choice = rps.find(el => el.name === e.target.id)
-    
     playRound(choice);
 }
 //Add event listeners to icons 
 choices.forEach(button => button.addEventListener('click', playerChoice));
-
+//Removes event listeners at a score of 5
+const gameOver = () => {
+    choices.forEach(button => button.removeEventListener('click', playerChoice));
+}
 const playRound = (player, cpu) => { //Takes the objects and passes them for winner evaluation
     this.player = player;
     cpu = cpuChoice();
         let playerWin = whoWins(player, cpu);
         let cpuWin = whoWins(cpu, player);
             if(playerWin) {
-                trackScore(playerScore)
+                trackScore(playerScore);
+                let text = `beats`;
+                showResult(player, text, cpu)
             }
             else if(cpuWin) {
-                trackScore(cpuScore)
+                trackScore(cpuScore);
+                let text = `beaten by`;
+                showResult(player, text, cpu)
             }
-            else draw()
+            else draw(player, cpu);
 }
 
 const whoWins = (winner, loser) => { //Evaluates winner
     return winner.beats === loser.name
 }
 
-const draw = () => { //Draw scenario
-    console.log(`draw`)
+const draw = (player, cpu) => { //Draw scenario
+    let text = `It's a draw`;
+    showResult(player, text, cpu)
 }
 
 const trackScore = (winner) => { //Takes winner score and adds 1
-    winner.innerText = parseInt(winner.innerText)+1
-    return winner.innerText
+    winner.innerText = parseInt(winner.innerText)+1;
+    if(parseInt(winner.innerText) === 5) gameOver();
+    return winner.innerText;
 }
+
+//Adds round outcome display
+let pHand = document.createElement('div');
+pHand.classList.add('pResult');
+    let cHand = document.createElement('div');
+    cHand.classList.add('cResult');
+        let textResult = document.createElement('div');
+        textResult.classList.add('textResult');
+        result.append(pHand, textResult, cHand);
+const showResult = (ply, text, cpu) => {
+    pHand.textContent = ply.icon;
+    cHand.textContent = cpu.icon;
+    textResult.textContent = text
+}
+
+//Restart button
+let button = document.createElement('button');
+button.classList.add('restart');
+button.textContent = 'Restart';
+
 })()
